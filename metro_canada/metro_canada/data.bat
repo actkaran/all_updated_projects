394980c07733b1c7bfba7a795e587e24bc9983af
goku_250 <karan.actowiz@gmail.com>
2024-11-19 10:42:03 +0530

    deleted metro project

--- metro_canada/metro_canada/data.bat
+++
@@ -1,5 +0,0 @@
-start "data_start_0_end_5600" scrapy crawl data -a start=0 -a end=5600
-start "data_start_5601_end_11200" scrapy crawl data -a start=5601 -a end=11200
-start "data_start_11201_end_16800" scrapy crawl data -a start=11201 -a end=16800
-start "data_start_16801_end_22400" scrapy crawl data -a start=16801 -a end=22400
-start "data_start_22401_end_28000" scrapy crawl data -a start=22401 -a end=28000
